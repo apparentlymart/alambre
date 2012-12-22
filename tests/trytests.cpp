f157ed992cf613ff1a55a56e615f0009b2593bb0
Martin Atkins <mart@degeneration.co.uk>
2012-12-22 10:59:40 -0800

    Remove the intentionally-failing dummy test.
    All of these dummy tests will go eventually, but removing this will
    hopefully make the Travis CI build succeed.

--- tests/trytests.cpp
+++ tests/trytests.cpp
@@ -5,6 +5,3 @@ TEST(TestTest, OnePlusOneIsTwo) {
     EXPECT_EQ(1 + 1, 2) << "one plus one is two";
 }
 
-TEST(TestTest, OnePlusTwoIsTwelve) {
-    EXPECT_EQ(1 + 2, 12) << "one plus two is twelve";
-}
