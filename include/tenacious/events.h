d204fc0de47b905b69e96b8ba36493255e71b07d
Martin Atkins <mart@degeneration.co.uk>
2013-02-03 18:58:34 -0800

    Support parameterized events.
    These events have one parameter in addition to the emitter. This could be
    useful for some events like one signalling a keypress from a keyboard
    and wanting to provide the scancode or ASCII character, or a sample from
    an ADC in free-run mode.

--- include/tenacious/events.h
+++ include/tenacious/events.h
@@ -6,15 +6,25 @@ inline void event_handler(typename EVENT_TYPE::emitter_type *emitter) {
     // do nothing by default
 }
 
+template<class EVENT_TYPE>
+inline void event_handler(typename EVENT_TYPE::emitter_type *emitter, typename EVENT_TYPE::param_type param) {
+    // do nothing by default
+}
+
 template <class EVENT_TYPE>
 inline void emit_event(typename EVENT_TYPE::emitter_type *obj) {
     event_handler<EVENT_TYPE>(obj);
 }
+template <class EVENT_TYPE>
+inline void emit_event(typename EVENT_TYPE::emitter_type *obj, typename EVENT_TYPE::param_type param) {
+    event_handler<EVENT_TYPE>(obj, param);
+}
 
-template <class EMITTER_TYPE>
+template <class EMITTER_TYPE, class PARAM_TYPE=void>
 class Event {
   public:
     typedef EMITTER_TYPE emitter_type;
+    typedef PARAM_TYPE param_type;
 };
 
 #endif
--- tests/events.cpp
+++ tests/events.cpp
@@ -1,6 +1,7 @@
 
 #include "tenacious/events.h"
 #include "gtest/gtest.h"
+#include <stdio.h>
 
 class Emitter {
 
@@ -14,6 +15,9 @@ class Emitter {
 
     class FrobEvent : public Event<Emitter> {};
     class WangleEvent : public Event<Emitter> {};
+    class UnhandledEvent : public Event<Emitter> {};
+    class ParamEvent : public Event<Emitter, int> {};
+    class UnhandledParamEvent : public Event<Emitter, int> {};
 
     void frob() {
         emit_event<FrobEvent>(this);
@@ -21,6 +25,18 @@ class Emitter {
 
     void wangle() {
         emit_event<WangleEvent>(this);
+    }
+
+    void unhandled() {
+        emit_event<UnhandledEvent>(this);
+    }
+
+    void param(int param) {
+        emit_event<ParamEvent>(this, param);
+    }
+
+    void unhandled_param(int param) {
+        emit_event<UnhandledParamEvent>(this, param);
     }
 };
 
@@ -32,6 +48,7 @@ Emitter emitter0(0);
 Emitter emitter1(1);
 int frob_count[2] = {0, 0};
 int wangle_count[2] = {0, 0};
+int param_event_param_sum = 0;
 
 template<>
 inline void event_handler<Emitter::FrobEvent>(Emitter *emitter) {
@@ -41,10 +58,18 @@ template<>
 void event_handler<typename Emitter::WangleEvent>(Emitter * emitter) {
     wangle_count[emitter->id]++;
 }
+template<>
+void event_handler<Emitter::ParamEvent>(Emitter * emitter, int param) {
+    param_event_param_sum += param;
+}
 
 TEST(TestEvents, Setup) {
     ::testing::StaticAssertTypeEq<Emitter, Emitter::FrobEvent::emitter_type>;
+    ::testing::StaticAssertTypeEq<void, Emitter::FrobEvent::param_type>;
     ::testing::StaticAssertTypeEq<Emitter, Emitter::WangleEvent::emitter_type>;
+    ::testing::StaticAssertTypeEq<void, Emitter::WangleEvent::param_type>;
+    ::testing::StaticAssertTypeEq<Emitter, Emitter::ParamEvent::emitter_type>;
+    ::testing::StaticAssertTypeEq<int, Emitter::ParamEvent::param_type>;
 }
 
 TEST(TestEvents, Emitting) {
@@ -59,10 +84,17 @@ TEST(TestEvents, Emitting) {
     emitter1.wangle();
     emitter1.wangle();
     emitter1.wangle();
+    emitter0.param(5);
+    emitter1.param(4);
 
     ASSERT_EQ(2, frob_count[0]);
     ASSERT_EQ(1, frob_count[1]);
     ASSERT_EQ(3, wangle_count[0]);
     ASSERT_EQ(4, wangle_count[1]);
+    ASSERT_EQ(9, param_event_param_sum);
 
+    // These two should be no-ops at runtime, but they're included
+    // here to make sure we can compile them.
+    emitter0.unhandled();
+    emitter0.unhandled_param(5);
 }
