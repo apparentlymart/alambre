2463e205f53b6b7200eb7fd7e157b8feb4b645aa
Martin Atkins <mart@degeneration.co.uk>
2013-02-17 22:34:56 -0800

    A (not quite working yet) attempt at the AVR SPI bus.

--- include/alambre/system/avr/spi.h
+++ include/alambre/system/avr/spi.h
@@ -11,14 +11,34 @@
 // system has a SPI Status Register and therefore supports SPI.
 #ifdef SPSR
 
+#ifndef MOSI_DDR
+#error avr-libc does not have MOSI_DDR defined for this target
+#endif
+#ifndef SCK_DDR
+#error avr-libc does not have SCK_DDR defined for this target
+#endif
+
 #include <alambre/bus/spi.h>
 
 class AvrSpiBus : public ISpiBus {
 
   public:
 
-    void shift_out(uint8_t value, BitOrder order) {
+    AvrSpiBus() {
+        MOSI_DDR |= 0x1 << MOSI_BIT;
+        SCK_DDR |= 0x1 << SCK_BIT;
+        SPCR |= (0x1 << SPE) | (0x1 << MSTR);
+    }
 
+    void shift_out(uint8_t value, BitOrder order) {
+        if (ISpiBus::LSB_FIRST) {
+            SPCR &= ~(0x1 << DORD);
+        }
+        else {
+            SPCR |= (0x1 << DORD);
+        }
+        SPDR = value;
+        while (!(SPSR & (1<<SPIF)));
     }
 
     uint8_t shift_in(BitOrder order) {
